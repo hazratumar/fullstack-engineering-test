f4cd20bd8080322784d6948a68675bb66bf356bc
hiumar88 <hiumar88@gmail.com>
2022-12-20 20:29:26 +0500

    first task control flow by callbacks hell method

--- controllers/Tasks.js
+++ controllers/Tasks.js
@@ -5,40 +5,75 @@ const { IWantTitle } = require("../operation/Scraper");
 
 //-------------------------------First Task----------------------------------------//
 
-module.exports.FirstTask = async (req, res) => {
+module.exports.FirstTask = (req, res) => {
     if (req.url.indexOf("address=") == -1) {
         errorHanding(res, "Please enter one or more addresses in URL")
         return;
     }
     htmlOpen(res);
-    tableHeader(res, "First Task : Control Flow By Pain Node Js");
+    tableHeader(res, "First Task : Control Flow By Pain Node Js Callbacks");
     let allFunctions = [];
     if (req.query.address instanceof Array) {
         const array = req.query.address
-        for (let address = 0; address < array.length; address++) {
-            const url = array[address];
-            allFunctions.push(new Promise((resolve) => {
-                IWantTitle(url, ((title, webURL) => {
-                    resolve([title, webURL])
-                }))
-            }))
+        for (let index = 0; index < array.length; index++) {
+            const url = array[index];
+            allFunctions.push((callback) => {
+                IWantTitle(url, (title, webURL) => {
+                    tableRow(res, title, webURL)
+                    callback()
+                })
+            })
         }
+        //------------------- Callback Hell Start --------------------//
+        // if i have one address
+        if (allFunctions.length === 1) {
+            allFunctions[0](() => {
+                tableClose(res);
+                htmlClose(res);
+            })
+            // else if i have two address
+        } else if (allFunctions.length === 2) {
+            allFunctions[0](() => {
+                allFunctions[1](() => {
+                    tableClose(res);
+                    htmlClose(res);
+                })
+            })
+            // else if i have three address
+        } else if (allFunctions.length === 3) {
+            allFunctions[0](() => {
+                allFunctions[1](() => {
+                    allFunctions[2](() => {
+                        tableClose(res);
+                        htmlClose(res);
+                    })
+                })
+            })
+            // else if i have four address
+        } else if (allFunctions.length === 4) {
+            allFunctions[0](() => {
+                allFunctions[1](() => {
+                    allFunctions[2](() => {
+                        allFunctions[3](() => {
+                            tableClose(res);
+                            htmlClose(res);
+                        })
+                    })
+                })
+            })
+        } else {
+            errorHanding(res, "Please enter addresses in the range 1 to 4. Because this control flow we have done with callbacks hell method. This is not good programming practice. But instead of callbacks hell we can use promise or async/await.")
+        }
+        //------------------- Callback Hell End--------------------//
+
     } else {
-        allFunctions.push(new Promise((resolve) => {
-            IWantTitle(req.query.address, ((title, webURL) => {
-                resolve([title, webURL])
-            }))
-        }))
+        IWantTitle(req.query.address, (title, webURL) => {
+            tableRow(res, title, webURL)
+            tableClose(res);
+            htmlClose(res);
+        })
     }
-    await Promise.all(allFunctions).then((itemArray) => {
-        itemArray.map((item) => {
-            tableRow(res, item[0], item[1])
-        });
-        tableClose(res);
-        htmlClose(res);
-    }).catch((error) => {
-        console.log("error", error)
-    })
+
 }
 
 
